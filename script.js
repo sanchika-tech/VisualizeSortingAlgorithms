7605e5254c8632cb684d9ea4f5bb4bd79a10c647
Sanchika Kumari <77576981+sanchika-tech@users.noreply.github.com>
2024-04-23 07:47:05 +0530

    script.js

--- script.js
+++ script.js
@@ -124,11 +124,29 @@ document.querySelector(".start").addEventListener("click", start);
 document.querySelector(".size-menu").addEventListener("change", RenderScreen);
 document.querySelector(".algo-menu").addEventListener("change", RenderScreen);
 window.onload = RenderScreen;
+function textFun(){
+  var selectedValue = this.value;
+  if (selectedValue === "0") {
+    document.querySelector(".description").innerHTML = "";
+  } else if (selectedValue === "1") {
+    document.querySelector(".description").innerHTML = "<h3>How Bubble Sort works</h3><br> <p>Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping adjacent elements if they are in the wrong order. This procedure is repeated until no swaps are required, indicating that the list has been sorted.</p> <br> <h3>Performance</h3><br> <p>Assuming N is the size of array,</p> <p>Worst time complexity<pre>O(N<sup>2</sup>)</pre></p> <p>Average time complexity<pre>θ(N<sup>2</sup>)</pre></p> <p>Best time complexity<pre>Ω(N)</pre></p> <p><br> <a href='https://www.geeksforgeeks.org/bubble-sort/'>Click for more information </a></p>";
+    document.querySelector(".description").style.backgroundColor = '#f48434';
+  } else if (selectedValue === "2") {
+    document.querySelector(".description").innerHTML = "<h3>How Selection Sort works</h3><br><p>Selection Sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning. The algorithm maintains two subarrays, a subarray which is sorted and the remaining array which is unsorted.</p><br> <h3>Performance</h3><br> <p>Assuming N is the size of array,</p> <p>Worst time complexity <pre> O(N<sup>2</sup>)</pre></p> <p>Average time complexity <pre> θ(N<sup>2</sup>)</pre></p> <p>Best time complexity<pre>Ω(N<sup>2</sup>)</pre></p> <p><br> <a href='https://www.geeksforgeeks.org/selection-sort/'>Click for more information </a></p>";
+    document.querySelector(".description").style.backgroundColor = '#f48434';
+  }
+  else if (selectedValue === "3") {
+    document.querySelector(".description").innerHTML = "<h3>How Insertion Sort works</h3><br><p>Insertion Sort is a simple sorting algorithm that builds the final sorted list one item at a time. The list is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.</p><br> <h3>Performance</h3><br> <p>Assuming N is the size of array,</p> <p>Worst time complexity <pre>O(N<sup>2</sup>)</pre></p> <p>Average time complexity <pre>θ(N<sup>2</sup>)</pre></p> <p>Best time complexity <pre>Ω(N)</pre></p> <p> <br><a href='https://www.geeksforgeeks.org/insertion-sort/'>Click for more information </a></p>";
+    document.querySelector(".description").style.backgroundColor = '#f48434';
+  }
+  else if (selectedValue === "4") {
+    document.querySelector(".description").innerHTML = "<h3>How Merge Sort works</h3><br><p>Merge Sort is a Divide and Conquer algorithm. It divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves. The merge() function is used for merging two halves. The merge(arr, l, m, r) is a key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted subarrays into one.</p><br> <h3>Performance</h3><br> <p>Assuming N is the size of array,</p> <p>Worst time complexity <pre>O(NlogN)</pre></p> <p>Average time complexity <pre>θ(NlogN)</pre></p> <p>Best time complexity  <pre>Ω(NlogN)</pre></p> <p><br> <a href='https://www.geeksforgeeks.org/merge-sort/'>Click for more information </a></p>";
+    document.querySelector(".description").style.backgroundColor = '#f48434';
+  }
+  else if (selectedValue === "5") {
+    document.querySelector(".description").innerHTML = "<h3>How Quick Sort works</h3><br><p>Quick Sort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. There are many different versions of Quick Sort that pick pivot in different ways.</p> <br><h3>Performance</h3><br> <p>Assuming N is the size of array,</p> <p>Worst time complexity <pre>O(N<sup>2</sup>)</pre></p> <p>Average time complexity <pre>θ(NlogN)</pre></p> <p>Best time complexity <pre>Ω(N)</pre></p> <p><br> <a href='https://www.geeksforgeeks.org/quick-sort/'>Click for more information </a></p>";
+    document.querySelector(".description").style.backgroundColor = '#f48434';
+  }
 
-
-
-
-  
-
-
-
+}
+document.getElementById("menu").addEventListener("change", textFun);
